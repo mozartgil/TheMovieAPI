1d188fb6c4e8f8086e313ede1d89b9ba61f5ad5a
Mozart Gil <mozartgil@gmail.com>
2022-07-11 17:36:17 -0300

    Added Get TV SHOWS List

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ class TheMovieApi
 
     public static void Main()
     {
+        var keyOption = "0";
+
         Console.Clear();
 
         Console.WriteLine("Authorizing session...");
@@ -21,8 +23,12 @@ class TheMovieApi
             Console.WriteLine("========================");
             Console.WriteLine("");
 
-            if (GetMenuOption() == "1") {
-                GetMovieListGenre().Wait();
+            keyOption = GetMenuOption();
+
+            if (keyOption == "1") {
+                GetGenres(1).Wait();
+            } else if (keyOption == "2") {
+                GetGenres(2).Wait();
             }
         }
     }
@@ -63,39 +69,57 @@ class TheMovieApi
         Console.WriteLine("");
 
         Console.WriteLine("1- Get MOVIE LIST Genre");
+        Console.WriteLine("2- Get TV SHOWS LIST Genre");
         Console.WriteLine("========================");
         Console.WriteLine("");
         Console.Write("Option: ");
         keyOption = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine("========================");
 
-        while (!Validation.ValidatesMenuInputNumber(1, 1, keyOption)) {
-            Console.WriteLine("");
+        while (!Validation.ValidatesMenuInputNumber(1, 2, keyOption)) {
             Console.WriteLine("Please choose a valid option");
             Console.WriteLine("");
             Console.WriteLine("Type 1 to get MOVIE LIST Genre");
+            Console.WriteLine("Type 2 to get TV SHOWS LIST Genre");
             Console.WriteLine("========================");
             Console.WriteLine("");
             Console.Write("Option: ");
             keyOption = Console.ReadLine();
+            Console.WriteLine("");
             Console.WriteLine("========================");
         }
 
         return keyOption;
     }
 
-    public static async Task GetMovieListGenre() {
+    public static async Task GetGenres(int genreKey) {
         var client = new HttpClient();
-        string BaseUrl = $"https://api.themoviedb.org/3/genre/movie/list?api_key={APIKey}&language=en-US";
+        var baseUrl = string.Empty;
+        var genreLabel = string.Empty;
+        var genreJSonFileName = string.Empty;
 
+        // 1 - MOVIES
+        // 2 - TV SHOWS
+        if (genreKey == 1) {
+            genreLabel = "MOVIES";
+            genreJSonFileName = "MoviesGenreList.json";
+            baseUrl = $"https://api.themoviedb.org/3/genre/movie/list?api_key={APIKey}&language=en-US";
+        } else if (genreKey == 2) {
+            genreLabel = "TV SHOWS";
+            genreJSonFileName = "TVShowsGenreList.json";
+            baseUrl = $"https://api.themoviedb.org/3/genre/tv/list?api_key={APIKey}&language=en-US";
+        }
+        
         try
         {
-             client.BaseAddress = new Uri(BaseUrl);
+             client.BaseAddress = new Uri(baseUrl);
              client.Timeout = new TimeSpan(0, 0, 90);
 
              HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
 
             if (!response.IsSuccessStatusCode) {
-                Console.WriteLine($"Falha ao tentar buscar pelos generos dos filmes!");
+                Console.WriteLine($"Something went wrong trying to get {genreLabel} genres. Try again soon!");
                 return;
             }
 
@@ -107,23 +131,38 @@ class TheMovieApi
             responseContent = responseContent.Trim(charsToTrim);
             responseContent = responseContent.Remove(responseContent.Length -1);
 
-            //Escrevendo o novo jogo
-            var nameMovieGenreListFile = "MovieGenreList.json";
-            File.Create(nameMovieGenreListFile).Close();
-            File.AppendAllText(nameMovieGenreListFile, responseContent);
+            //WRITING GENRE JSON
+            File.Create(genreJSonFileName).Close();
+            File.AppendAllText(genreJSonFileName, responseContent);
 
-            var listMovieGenres = new List<MovieGenre>();
-            var strMovieGenresList = File.ReadAllText(nameMovieGenreListFile);
+            if (genreKey == 1) {
+                var listGenres = new List<MovieGenre>();
+                var strGenresList = File.ReadAllText(genreJSonFileName);
 
-            listMovieGenres = JsonSerializer.Deserialize<List<MovieGenre>>(strMovieGenresList);
+                listGenres = JsonSerializer.Deserialize<List<MovieGenre>>(strGenresList);
 
-            Console.WriteLine("Official GENRE list for MOVIES");
-            Console.WriteLine("===============================");
+                Console.WriteLine($"Official GENRE list for {genreLabel}");
+                Console.WriteLine("===============================");
 
-            foreach (var genre in listMovieGenres)
-            {
-                Console.WriteLine(genre.name);
+                foreach (var genre in listGenres)
+                {
+                    Console.WriteLine(genre.name);
+                }
+            } else if (genreKey == 2) {
+                var listGenres = new List<TVShowGenre>();
+                var strGenresList = File.ReadAllText(genreJSonFileName);
+
+                listGenres = JsonSerializer.Deserialize<List<TVShowGenre>>(strGenresList);
+
+                Console.WriteLine($"Official GENRE list for {genreLabel}");
+                Console.WriteLine("===============================");
+
+                foreach (var genre in listGenres)
+                {
+                    Console.WriteLine(genre.name);
+                }
             }
+            
         }
         catch (System.Exception e)
         {
@@ -136,6 +175,15 @@ class TheMovieApi
         public int id { get; }
         public string? name { get; }
         public MovieGenre(int id, string name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    class TVShowGenre {
+        public int id { get; }
+        public string? name { get; }
+        public TVShowGenre(int id, string name) {
             this.id = id;
             this.name = name;
         }
